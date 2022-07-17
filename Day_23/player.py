df90cd8d7fcc215b2940c7f9bdac4972ba719896
dennisephraim <akainetteyephraim1234@gmail.com>
2022-07-17 14:24:17 +0000

    Finished the capstone project

--- Day_23/player.py
+++ Day_23/player.py
@@ -7,10 +7,21 @@ FINISH_LINE_Y = 280
 
 class Player(Turtle):
 
-    def __abs__(self):
+    def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
